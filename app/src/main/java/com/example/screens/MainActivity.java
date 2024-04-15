fffccf4ee949aa3b9cecc1d1f951ccf7e0eea0c5
a23matsh <a23matsh@student.his.se>
2024-04-15 15:41:32 +0200

    Added parentActivityName for a backarrow that will lead back to mainactivity from second. Also added code in Java to enable button leading to second activity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonToStartSecondActivity = findViewById(R.id.buttonToStartSecondActivity);
+        buttonToStartSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", "Hej detta är test"); // Optional
+                intent.putExtra("number", 100); // Optional
+                startActivity(intent);
+
+
+            }
+
+
+        });
     }
 }
