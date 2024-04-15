4d1eb8b7b875f75f97c9c0c76e64a02c398895c8
a23matsh <a23matsh@student.his.se>
2024-04-15 16:12:06 +0200

    Have set bundle extras and added 2 new textviews (textView & textView2) but this time in activity_second that will display some sort of text when opened .

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.screens;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,18 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("Hello");
+            int number = extras.getInt("1000");
+            TextView textView = findViewById(R.id.textView);
+            TextView textView2 = findViewById(R.id.textView2);
+            textView.setText(name);
+            textView.setText("" + number);
+
+
+        }
 
     }
 }
